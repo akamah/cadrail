f01b73a06db4382821514aeff3b0eb62d5ee0a9d
Akama Hitoshi <akamahitoshi@gmail.com>
2017-08-22 00:35:50 +0900

    testing models

--- src/RailView.ts
+++ src/RailView.ts
@@ -4,8 +4,7 @@ export class RailView {
     private renderer: THREE.WebGLRenderer;
     private scene:    THREE.Scene;
     private camera:   THREE.Camera;
-    
-    private rail:     THREE.Mesh;
+    private controls: THREE.OrbitControls;
     
     constructor() {
         this.initRenderer();
@@ -20,29 +19,30 @@ export class RailView {
     }
     
     private initCamera() {
-        const radius = 10;
+        const radius = 300;
         const ratio = window.innerWidth / window.innerHeight;
         const w = radius * ratio;
         const h = radius;
-
+        
         this.camera = new THREE.OrthographicCamera(-w, w, h, -h);
-        this.camera.position.set(100, 100,  500);
-        this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+        this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+
     }
     
+    private load(path: string, color: number) {
+        var loader = new THREE.JSONLoader();
+
+        var material = new THREE.MeshLambertMaterial({ color: color});
+
+        loader.load('./build/assets/' + path, (geometry, materials) => {
+            var rail = new THREE.Mesh(geometry, material);
+            this.scene.add(rail);
+        });
+    }
 
     private initScene() {
         this.scene = new THREE.Scene();
             
-        var loader = new THREE.JSONLoader();
-
-        loader.load('./model/straight.json', (geometry, materials) => {
-            this.rail = new THREE.Mesh(geometry, materials[0]);
-            this.scene.add(this.rail);
-            this.rail = new THREE.Mesh(geometry, materials[0]);
-            this.rail.position.x = 216;
-            this.scene.add(this.rail);
-        });
         const light = new THREE.PointLight();
         light.position.set(200, 400, -100);
         
@@ -52,11 +52,27 @@ export class RailView {
         const axisHelper = new THREE.AxisHelper(50);
         this.scene.add(axisHelper);
         
+        const blue = 0x3399FF;
+        const gray = 0x666666;
+
+        this.load('autopoint_a.json', blue);
+        this.load('autopoint_b.json', gray);
+        this.load('autopoint_c.json', gray);
+        this.load('autopoint_ab.json', blue);
+        this.load('autopoint_bc.json', gray);
+        this.load('autopoint_abc.json', blue);
+        
     }
     
     public render() {
         window.requestAnimationFrame(this.render.bind(this));
         
+       /*  if (this.rail != null) {
+            this.rail.rotation.x += 0.01;
+            this.rail.rotation.z += 0.005;
+
+        } */
+
         this.renderer.render(this.scene, this.camera);
     }
 }
