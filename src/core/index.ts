cd9bdbf45428feb65ce2e4c93759fb1ef10c0e0d
Akama Hitoshi <akamahitoshi@gmail.com>
2017-09-17 21:08:09 +0900

    remove core files

--- src/core/index.ts
+++
@@ -1,7 +0,0 @@
-import { Dir } from "./Dir";
-import { End } from "./End";
-import { Point } from "./Point";
-import { Pole } from "./Pole";
-import { Rot } from "./Rot";
-
-export { Dir, End, Point, Pole, Rot }
